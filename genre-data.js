f6fd93458441acb81b75de04d39e6238edd39fd6
nana <edsheeran1022@yahoo.co.jp>
2025-10-08 13:03:40 +0900

    ジャンルページを追加

---
+++ genre-data.js
@@ -0,0 +1,43 @@
+const genreData = {
+
+  celebrity: {
+    title: "有名人",
+    image: "celebrity1.jpg",
+    description: [
+        "有名人です",
+    ]
+  },
+
+  vehicle: {
+    title: "自動車",
+    image: "vehicle1.jpg",
+    description: [
+        "自動車です。",
+    ]
+  },
+
+  place: {
+    title: "観光地",
+    image: "place1.jpg",
+    description: [
+        "観光地です。",
+    ]
+  },
+
+  food: {
+    title: "名古屋めし",
+    image: "food1.jpg",
+    description: [
+        "名古屋めしです。",
+    ]
+  },
+
+  history: {
+    title: "歴史",
+    image: "history1.jpg",
+    description: [
+        "歴史です。",
+    ]
+  },
+
+};
