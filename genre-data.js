faacad404607b34538244ad124466470ba585206
nana <edsheeran1022@yahoo.co.jp>
2025-10-08 14:15:45 +0900

    内容を更新しました

--- genre-data.js
+++ genre-data.js
@@ -2,41 +2,84 @@ const genreData = {
 
   celebrity: {
     title: "有名人",
-    image: "celebrity1.jpg",
-    description: [
-        "有名人です",
+    images: [
+        "celebrity1.JPG",
+        "celebrity2.JPG",
+        "celebrity3.JPG",
+        "celebrity4.JPG",
+        "celebrity5.JPG",
+        "celebrity6.JPG",
+        "celebrity7.JPG",
+        "celebrity8.JPG",
     ]
+    
   },
 
   vehicle: {
     title: "自動車",
-    image: "vehicle1.jpg",
-    description: [
-        "自動車です。",
+    images: [
+        "vehicle1.JPG",
+        "vehicle2.JPG",
+        "vehicle3.JPG",
+        "vehicle4.JPG",
+        "vehicle5.JPG",
+        "vehicle6.JPG",
+        "vehicle7.JPG",
+        "vehicle8.JPG",
+        "vehicle9.JPG",
+        "vehicle10.JPG",
     ]
   },
 
   place: {
     title: "観光地",
-    image: "place1.jpg",
-    description: [
-        "観光地です。",
+    images: [
+        "place1.JPG",
+        "place2.JPG",
+        "place3.JPG",
+        "place4.JPG",
+        "place5.JPG",
+        "place6.JPG",
+        "place7.JPG",
+        "place8.JPG",
+        "place9.JPG",
+        "place10.JPG",
+        "place11.JPG",
+        "place12.JPG",
+        "place13.JPG",
+        "place14.JPG",
     ]
   },
 
   food: {
     title: "名古屋めし",
-    image: "food1.jpg",
-    description: [
-        "名古屋めしです。",
+    images: [
+        "food1.JPG",
+        "food2.JPG",
+        "food3.JPG",
+        "food4.JPG",
+        "food5.JPG",
+        "food6.JPG",
+        "food7.JPG",
+        "food8.JPG",
+        "food9.JPG",
+        "food10.JPG",
     ]
   },
 
   history: {
     title: "歴史",
-    image: "history1.jpg",
-    description: [
-        "歴史です。",
+    images: [
+        "history1.JPG",
+        "history2.JPG",
+        "history3.JPG",
+        "history4.JPG",
+        "history5.JPG",
+        "history6.JPG",
+        "history7.JPG",
+        "history8.JPG",
+        "history9.JPG",
+        "history10.JPG",
     ]
   },
 
